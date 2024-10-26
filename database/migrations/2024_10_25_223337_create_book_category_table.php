aabb55d88541330675b049124d29a5b5f828a595
manueledigregorio <dig.manuele@gmail.com>
2024-10-26 04:20:57 +0200

    Implement many-to-many relationship between books and categories

--- database/migrations/2024_10_25_223337_create_book_category_table.php
+++ database/migrations/2024_10_25_223337_create_book_category_table.php
@@ -11,11 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('book_category', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->text('description')->nullable();
-            $table->string('image_path')->nullable();
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->foreignId('book_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -25,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('book_category');
     }
 };
